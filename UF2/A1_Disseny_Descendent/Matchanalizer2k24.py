03c807bfb9659e77da38ddd3c367a2a0cd54c13b
sjo <eric.pasto.7e4@itb.cat>
2024-02-08 09:38:07 +0100

    j

---
+++ UF2/A1_Disseny_Descendent/Matchanalizer2k24.py
@@ -0,0 +1,10 @@
+"""
+Eric Pastó
+ASIXc1C
+08/02/2024
+MatchAnalizer - Volem fer un programa que analitzi els resultats d'un partit de bàsquet.
+Tenir en compte que en un partit de bàsquet es poden fer 1, 2 o 3 punts.
+Donat els diferents resultats analitza que està passant.
+L'usuari introduirà primer el nom dels dos equips i després el resultat actual
+"""
+
