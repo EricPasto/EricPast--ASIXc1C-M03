3530db387bbf815d298288ac67a20692122c6113
eric.pasto.7e4 <eric.pasto.7e4@itb.cat>
2024-02-13 10:42:36 +0100

    l

--- UF2/A1_Disseny_Descendent/Matchanalizer2k24.py
+++ UF2/A1_Disseny_Descendent/Matchanalizer2k24.py
@@ -8,3 +8,21 @@ Donat els diferents resultats analitza que està passant.
 L'usuari introduirà primer el nom dels dos equips i després el resultat actual
 """
 
+equip1 = ""
+equip2 = ""
+puntse1 = 0
+puntse2 = 0
+
+#obtenirEquips
+def obtenirEquips():
+    equip1 = print (input ("Nom de l'equip 1? "))
+    equip2 = print (input ("Nom de l'equip 2? "))
+    return equip1, equip2
+#obtenir resultats
+def obtenirResultats():
+    print("hola")
+#procesar resultats
+#tractar errors
+
+obtenirEquips()
+print ("els equips son: ",equip1, equip2)
