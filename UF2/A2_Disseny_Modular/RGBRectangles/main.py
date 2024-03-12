08df1a0be02ba578239dc59ee92bc9152681c8c1
eric.pasto.7e4 <eric.pasto.7e4@itb.cat>
2024-03-12 11:50:32 +0100

    laa

--- UF2/A2_Disseny_Modular/RGBRectangles/main.py
+++ UF2/A2_Disseny_Modular/RGBRectangles/main.py
@@ -23,18 +23,20 @@ def crearforma(color, ancho, alto):
     except KeyError:
         print("El color que acabas de introducir no está en la lista")
 
-# Solicitar rectángulos hasta que se ingrese ";q" o ";Q"
-while True:
-    pedirRectangulo = input("Introduce el rectángulo (color ancho largo): ")
-    if pedirRectangulo.lower() in (';q', ';Q'):
-        break
-    paraules = pedirRectangulo.split()
-    color = paraules[0]
-    ancho = int(paraules[1])
-    alto = int(paraules[2])
-    crearforma(color, ancho, alto)
+def solicitarRectangulos():
+    while True:
+        pedirRectangulo = input("Introduce el rectángulo (color ancho largo): ")
+        if pedirRectangulo.lower() in (';q', ';Q'):
+            break
+        paraules = pedirRectangulo.split()
+        color = paraules[0]
+        ancho = int(paraules[1])
+        alto = int(paraules[2])
+        crearforma(color, ancho, alto)
 
-# Imprimir todos los rectángulos
-for r in rectangulos:
-    print(r)
+def imprimirRectangulos():
+    for r in rectangulos:
+        print(r)
 
+solicitarRectangulos()
+imprimirRectangulos()
