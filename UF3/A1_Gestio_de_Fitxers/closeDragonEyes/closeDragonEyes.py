cb43c0e7588e34cdef42608b4d698845f77b29a4
Eric Pastó <113977250+EricPasto@users.noreply.github.com>
2024-04-11 07:26:02 +0000

    a

--- UF3/A1_Gestio_de_Fitxers/closeDragonEyes/closeDragonEyes.py
+++ UF3/A1_Gestio_de_Fitxers/closeDragonEyes/closeDragonEyes.py
@@ -9,6 +9,5 @@ with open("DragonEyesClosed.out", "w") as f:
     f.write(contenido_modificado)
 
 
-print("Se ha creado el archivo Dragon_out.txt con los 0 reemplazados por X.")
-
+print("Se ha creado el archivo con los ojos cerrados.")
 
--- UF3/A1_Gestio_de_Fitxers/closeKnightsEyes/closeKnightsEyes.py
+++ UF3/A1_Gestio_de_Fitxers/closeKnightsEyes/closeKnightsEyes.py
@@ -9,4 +9,4 @@ with open("KnightEyesClosed.out", "w") as f:
     f.write(contenido_modificado)
 
 
-print("Se ha creado el archivo Dragon_out.txt con los 0 reemplazados por X.")
+print("Se ha creado el archivo con los ojos cerrados.")
