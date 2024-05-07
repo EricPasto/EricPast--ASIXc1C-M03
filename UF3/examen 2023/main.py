6a4d467f05b6f67ccfb4da746656221ea975614e
eric.pasto.7e4 <eric.pasto.7e4@itb.cat>
2024-05-07 11:48:55 +0200

    a

--- UF3/examen 2023/main.py
+++ UF3/examen 2023/main.py
@@ -1,11 +1,20 @@
-import paraules
-import paraulesvocals
 import logging
 import os
+import paraules
+import paraulesvocals
 
-paraules.guardar_paraules_per_longitud("paraules.txt", "paraules_longitud", "sortida")
+# Configuración del registro de eventos
+logging.basicConfig(filename='main.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-paraulesvocals.copiar_i_afegir_vocals("paraules.txt", "paraulesQuantitatVocals.txt")
+# Llamadas a las funciones de los módulos paraules y paraulesvocals
+try:
+    paraules.guardar_paraules_per_longitud("paraules.txt", "paraules_longitud", "sortida")
+    logging.info("Se han guardado las palabras por longitud correctamente.")
+except Exception as e:
+    logging.error(f"Error al guardar las palabras por longitud: {str(e)}")
 
-
-
+try:
+    paraulesvocals.copiar_i_afegir_vocals("paraules.txt", "paraulesQuantitatVocals.txt")
+    logging.info("Se ha creado el archivo 'paraulesQuantitatVocals.txt' correctamente.")
+except Exception as e:
+    logging.error(f"Error al crear el archivo 'paraulesQuantitatVocals.txt': {str(e)}")
